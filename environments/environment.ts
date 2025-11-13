4c7b57132f43e3b6d470b645223902fadfb5bb1d
Matheus Sathler <mattsathler@gmail.com>
2025-11-13 15:32:50 -0300

    fix: change environment to prod

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    // API_URL: 'https://syn-api-9pgn.onrender.com',
-    API_URL: 'http://localhost:3000',
+    API_URL: 'https://syn-api-9pgn.onrender.com',
+    // API_URL: 'http://localhost:3000',
 };
